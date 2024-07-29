5ea933cf7889989daa46d86038ec5460bf1da44c
Sreejith Aji <sreejithaji27@gmail.com>
2024-07-29 20:59:04 +0530

    login code

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,7 @@ from website import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('cart', views.cart, name='cart'),
-    path('checkout', views.checkout, name='checkout')
+    path('checkout', views.checkout, name='checkout'),
+    path('login', views.login, name='login'),
+    path('contact', views.contact, name='contact')
 ]
--- website/views.py
+++ website/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+
 
 
 # Create your views here.
@@ -12,3 +15,28 @@ def cart(request):
 
 def checkout(request):
     return render(request, 'checkout.html')
+
+def contact(request):
+    return render(request, 'contact-us.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if email and password and username:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'email already exists')
+                return redirect('login')
+
+            else:
+                user = User.objects.create_user(name=name, email=email, password=password, username=username)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'all fields required')
+    else:
+        return render(request, 'login.html')
